f74daedafbae0c9cfac8df01f0beb0cb1701705c
Donald Williams <60931728+Don-Williams@users.noreply.github.com>
2022-04-17 19:46:04 -0600

    added code to print to vga buffer when booted

--- rost/src/main.rs
+++ rost/src/main.rs
@@ -7,7 +7,18 @@ fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
+static HELLO: &[u8] = b"Hello World!";
+
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
+    let vga_buffer = 0xb8000 as *mut u8; // cast address of vga buffer to a pointer
+
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte; // writes byte in HELLO string
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb; // writes byte controlling color of text
+        }
+    }
+
     loop {}
 }
