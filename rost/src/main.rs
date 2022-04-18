3eae0fb9c181136c4bca4d169a8bbb5301310b51
Donald Williams <60931728+Don-Williams@users.noreply.github.com>
2022-04-18 08:57:58 -0600

    implemented custom test framework and test entry

--- rost/src/main.rs
+++ rost/src/main.rs
@@ -1,11 +1,14 @@
 #![no_std]
 #![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     println!("{}", info);
-    
+
     loop {}
 }
 
@@ -15,7 +18,21 @@ static HELLO: &[u8] = b"Hello World!";
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
     println!("Hello World{}", "!");
-    panic!("Some panic message");
-
+    // panic!("Some panic message");
+    
+    #[cfg(test)]
+    test_main();
+    
     loop {}
+}
+
+
+
+
+#[cfg(test)]
+fn test_runner(tests: &[&dyn Fn()]) {
+    println!("Running {} tests", tests.len());
+    for test in tests {
+        test();
+    }
 }
