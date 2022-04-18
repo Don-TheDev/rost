7b232052990fa3b29a3a9afc0243276e44251fba
Donald Williams <60931728+Don-Williams@users.noreply.github.com>
2022-04-18 08:22:15 -0600

    added lazy static implementation of writer

--- rost/src/vga_buffer.rs
+++ rost/src/vga_buffer.rs
@@ -138,3 +138,13 @@ impl Writer {
         }
     }
 }
+
+use lazy_static::lazy_static;
+
+lazy_static! {
+    pub static ref WRITER: Writer = Writer {
+        column_position: 0,
+        color_code: ColorCode::new(Color::Yellow, Color::Black),
+        buffer: unsafe { &mut *(0xb8000 as *mut Buffer) },
+    };
+}
