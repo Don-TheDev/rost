642d9ce5945bcf7436edf883c79da5a4ee258772
Donald Williams <60931728+Don-Williams@users.noreply.github.com>
2022-04-20 11:57:04 -0600

    implemented coop concurrency

--- rost/src/main.rs
+++ rost/src/main.rs
@@ -9,14 +9,18 @@ extern crate alloc;
 use alloc::{boxed::Box, vec, vec::Vec, rc::Rc};
 use bootloader::{BootInfo, entry_point};
 use core::panic::PanicInfo;
-use rost::{println};
+use rost::{println, task::{Task, executor::Executor}};
 
 
 entry_point!(kernel_main);
 
 fn kernel_main(boot_info: &'static BootInfo) -> ! {
-    use rost::allocator;
-    use rost::memory::{self, BootInfoFrameAllocator};
+    use rost::
+    {
+        allocator,
+        memory::{self, BootInfoFrameAllocator},
+        task::keyboard,
+    };
     use x86_64::{structures::paging::{Page, Translate}, VirtAddr};
 
     println!("Hello World{}", "!");
@@ -31,26 +35,25 @@ fn kernel_main(boot_info: &'static BootInfo) -> ! {
     allocator::init_heap(&mut mapper, &mut frame_allocator)
         .expect("heap initialization failed");
 
-    let heap_value = Box::new(41);
-    println!("heap value at {:p}", heap_value);
-
-    let mut vec = Vec::new();
-    for i in 0..500 {
-        vec.push(i);
-    }
-    println!("vec at {:p}", vec.as_slice());
-
-    let reference_counted = Rc::new(vec![1, 2, 3]);
-    let cloned_reference = reference_counted.clone();
-    println!("current reference count is {}", Rc::strong_count(&cloned_reference));
-    core::mem::drop(reference_counted);
-    println!("current reference count is {} now", Rc::strong_count(&cloned_reference));
+    let mut executor = Executor::new();
+    executor.spawn(Task::new(example_task()));
+    executor.spawn(Task::new(keyboard::print_keypresses()));
+    executor.run();
 
     #[cfg(test)]
     test_main();
     
     println!("It did not crash!");
     rost::hlt_loop();
+}
+
+async fn async_number() -> u32 {
+    42
+}
+
+async fn example_task() {
+    let number = async_number().await;
+    println!("async number: {}", number);
 }
 
 #[cfg(not(test))]
